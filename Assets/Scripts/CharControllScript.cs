74141563d99d748860b296036632402751372643
Unknown <noelledino@gmail.com>
2017-02-06 15:38:37 -0500

    NPC Dialog Text Box
    can press Z to make a string of text appear.  Can press Z to end the conversation with the NPC.

---
+++ Assets/Scripts/CharControllScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharControllScript : MonoBehaviour{
+
+	public float maxSpeed = 15f;
+	bool facingRight = true;
+	Animator anim;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = .02f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 800f;
+	bool doubleJump = false; 
+
+	void Start ()	{
+		anim = GetComponent<Animator> ();
+		GetComponent<Rigidbody2D> ().freezeRotation = true;
+	}
+
+	void FixedUpdate ()	{
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+
+		anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
+		float move = Input.GetAxis ("Horizontal"); 
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+
+	void Update (){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (grounded) {
+				anim.SetBool ("Ground", false);
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, 0f);   
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0f, jumpForce)); 
+				doubleJump = true;
+			} else if (doubleJump) {
+				doubleJump = false;
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, 0f);  
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0f, jumpForce)); 
+			}
+		}
+	}
+
+void Flip () {
+	facingRight = !facingRight;
+	Vector3 theScale = transform.localScale;
+	theScale.x *= -1;
+	transform.localScale = theScale;
+}
+}
