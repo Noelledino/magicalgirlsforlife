10089587a14449d27ac0e1337ab957d2dacaf756
Unknown <noelledino@gmail.com>
2017-02-27 01:38:31 -0500

    Clickable Objects Fixed and Committed
    actually commited that script this time since apparently i never did last week

---
+++ Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectScript : MonoBehaviour
+{
+	private Renderer rend;
+
+
+	void  Start ()
+	{
+		rend = GetComponent<Renderer> ();
+	}
+
+	void OnMouseEnter ()
+	{
+		rend.material.color -= new Color (10F, 10, 10) * Time.deltaTime;
+		AudioSource ourAudio = GetComponent<AudioSource> ();
+
+	}
+
+	void OnMouseDown ()
+	{ 
+		Debug.Log ("An object has been clicked.");
+	}
+
+	void OnMouseExit ()
+	{
+		rend.material.color = Color.white;
+
+	}
+
+}
